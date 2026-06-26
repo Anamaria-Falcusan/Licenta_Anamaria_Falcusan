3ec9f85afe551aafce27daa01b480673134f3090
Anamaria-Falcusan <anamaria.falcusan@gmail.com>
2026-06-26 10:46:18 +0300

    logout_admin.php

--- admin/logout_admin.php
+++ admin/logout_admin.php
@@ -1,7 +1,6 @@
 <?php
 require_once __DIR__ . "/../config/init.php";
 
-// Scoatem doar organizatorul din sesiune (nu afectăm spectator/artist dacă sunt logați în paralel)
 unset($_SESSION["admin_id"]);
 
 header("Location: " . BASE_URL . "/admin/login_admin.php");
